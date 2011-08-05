01f99f056defe587ca80ce051eebaaadc293d7d9
shim <shim@bebop>
2011-08-05 08:44:58 -0500

    Correctly spell Character

--- BattleNetAPI/WoW/Guild.cs
+++ BattleNetAPI/WoW/Guild.cs
@@ -46,14 +46,14 @@ namespace BattleNet.API.WoW
     {
         [XmlElement("character")]
         [DataMember(Name = "character")]
-        public Character Characater { get; set; }
+        public Character Character { get; set; }
         [XmlElement("rank")]
         [DataMember(Name = "rank")]
         public int Rank { get; set; }
 
         public override string ToString()
         {
-            return "{" + this.Characater.Name + " " + Rank + "}";
+            return "{" + this.Character.Name + " " + Rank + "}";
         }
     }
 
